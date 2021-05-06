f1e0760d46544c8355c0c7f7b4735ec38af620e0
sagar bapardekar <sagar.bapardekar@globalpricing.com>
2021-05-06 19:48:39 +0530

    proxy config added

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,37 +1,38 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient , HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class DataService {
+
   private _districtsJsonURL = 'assets/districts.json';
   private _baseURL = 'api/';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http:HttpClient) { }
 
-  public getAllDisticts(): Observable<any> {
-    return this.http.get(this._districtsJsonURL);
+  public getAllDisticts(): Observable<any>{
+      return this.http.get(this._districtsJsonURL)
   }
 
   /**
    * getVaccineAvailability
      districtId,availabilityDate   */
-  public getVaccineAvailability(districtId, availabilityDate): Observable<any> {
-    let finalUrl = `${environment.apiUrl}/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${availabilityDate}`;
-
-    // if (environment.production) {
-    //   finalUrl = `${environment.apiUrl}/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${availabilityDate}`;
+  public getVaccineAvailability (districtId,availabilityDate): Observable<any> {
+    let finalUrl = `/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${availabilityDate}`
+    
+    // if (environment.production){
+    //   finalUrl = `${environment.apiUrl}/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${availabilityDate}`
     // }
 
-    return this.http.get(finalUrl, {
+    return this.http.get(finalUrl,{
       headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Accept-Language': 'en_US',
-        'Access-Control-Allow-Origin': '*',
-      }),
-    });
+        'Content-Type':  'application/json',
+        'Accept-Language':'en_US',
+        'Access-Control-Allow-Origin':'*'
+      })
+    })
   }
 }
